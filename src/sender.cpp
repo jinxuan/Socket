e8f5a8173fb496d39f97685ae279f4c759530a23
jinxuan wu <jinxuanw@asu.edu>
2015-05-19 16:26:59 -0700

    Second commit

--- src/sender.cpp
+++ src/sender.cpp
@@ -26,6 +26,18 @@ int main(int argc, char * argv[]){
     inet_pton(AF_INET, ip,  &server_address.sin_addr);
     server_address.sin_port = htons(port);
 
-
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sockfd > 0);
+    if(connect(sockfd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+        printf("Connection failed\n");
+    }
+    else {
+        const char* oob_data = "abc";
+        const char* normal_data = "123";
+        send(sockfd, normal_data, strlen(normal_data), 0);
+        send(sockfd, oob_data, strlen(oob_data), MSG_OOB);
+        send(sockfd, normal_data, strlen(normal_data), 0);
+    }
+    close(sockfd);
     return 0;
 }
